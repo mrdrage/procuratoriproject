626196f1dc642294de8482b24a879e1bc38df778
Michele Ragone <mi.ragone@studenti.unina.it>
2021-03-04 23:56:05 +0100

    Server connesso!

--- GestioneProcuratori/src/DBconfig/DBConnection.java
+++ GestioneProcuratori/src/DBconfig/DBConnection.java
@@ -15,10 +15,10 @@ public class DBConnection
 	private static DBConnection instance;
 	private Connection connection = null;
 	private final String USERNAME = "dbproject";
-	private final String PASSWORD = "dbprogectadmin";
+	private final String PASSWORD = "dbprojectadmin";
 	private final String IP = "dbprocuratori1.c1r3woxfz75h.us-east-2.rds.amazonaws.com";
 	private final String PORT = "5432";
-	private String url = "jdbc:postgresql://" + IP + ":" + PORT + "/ProgettoOOBD";
+	private String url = "jdbc:postgresql://" + IP + ":" + PORT + "/dbprocuratori";
 
 	private DBConnection() throws SQLException
 	{
