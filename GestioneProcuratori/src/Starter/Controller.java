96e8debae686c9510d64a154231d3d9427737fa8
Salvatore Mastrominico <s.mastrominico@studenti.unina.it>
2021-03-05 12:09:10 +0100

    pulit il controller

--- GestioneProcuratori/src/Starter/Controller.java
+++ GestioneProcuratori/src/Starter/Controller.java
@@ -12,21 +12,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 		
-//		try {
-//			Class.forName("org.postgres.Driver");
-//		}catch(ClassNotFoundException e) {
-//			System.out.println ("non trovata classe");
-//		}
-		
- 
-		try {
-			DBConnection dbconnection = DBConnection.getInstance();
-			 
-			Connection conn = dbconnection.getConnection(); 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 		
 		
 	}
