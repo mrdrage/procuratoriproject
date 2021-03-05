2932f51c9f09ed5f04e55f90785f21a65c7836f8
Michele Ragone <mi.ragone@studenti.unina.it>
2021-03-05 12:42:20 +0100

    Server connected

--- GestioneProcuratori/src/Starter/Controller.java
+++ GestioneProcuratori/src/Starter/Controller.java
@@ -4,11 +4,14 @@ import java.sql.SQLException;
 
 
 import java.sql.Connection;
+import java.rmi.AccessException;
 import java.sql.*;
 
 import DBconfig.DBConnection;
 
 public class Controller {
+	
+		
 	
 	public void IniziaRicerca () {
 		
@@ -16,10 +19,17 @@ public class Controller {
 	
 
 	public static void main(String[] args) {
-		
 
-		
+		try{
+			DBConnection newdbconnection = DBConnection.getInstance();
+			Connection pon = newdbconnection.getConnection();
+		}
+			catch(SQLException e){
+				
+			}
 		
 	}
 
+
+	
 }
